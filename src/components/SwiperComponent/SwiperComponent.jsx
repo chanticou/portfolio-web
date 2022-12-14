2f001a6f4c094028bc5d427499900cf46d701edd
Chantal <chanticou@gmail.com>
2022-12-14 14:04:40 -0300

    second commit

--- src/components/SwiperComponent/SwiperComponent.jsx
+++ src/components/SwiperComponent/SwiperComponent.jsx
@@ -1,96 +1,232 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import propertiesAndYou from "../../assets/propertiesandyou.jpg";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
-import "./SwiperComponent.css";
-// import required modules
-import { Pagination } from "swiper";
 import { Link } from "react-router-dom";
+import propertiesAndYou from "../../assets/propertiesandyou.jpg";
+import chiaoLogo from "../../assets/chiao.jpg";
+import "./SwiperComponent.css";
 
 export function SwiperComponent() {
   return (
     <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="content_swiperSlide_swiperComponent">
-            {/* <Link to="/proyect/propertiesAndYou"> */}
-            <p>Properties&&you</p>
-            <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+      <div className="wrapper">
+        <section id="section1">
+          <a href="#section3" className="arrow__btn left-arrow">
+            ‹
+          </a>
+          <div className="item">
+            <a href="https://proyecto-final-frontend-lyart.vercel.app/">
               <img
-                className="image_propertiesAndYou_swiperComponent"
+                className="image_carrusel"
                 src={propertiesAndYou}
-                alt=""
-              />
-            </a>
-          </div>
-        </SwiperSlide>
+                alt="Describe Image"
+              ></img>
 
-        <SwiperSlide>
-          <div className="content_swiperSlide_swiperComponent">
-            {/* <Link to="/proyect/propertiesAndYou"> */}
-            <p>Properties&&you</p>
-            <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
-              <img
-                className="image_propertiesAndYou_swiperComponent"
-                src={propertiesAndYou}
-                alt=""
-              />
+              <h1 className="heading">Properties&&you</h1>
+              <p className="duration">Proyecto final soyHenry</p>
             </a>
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="content_swiperSlide_swiperComponent">
-            {/* <Link to="/proyect/propertiesAndYou"> */}
-            <p>Properties&&you</p>
-            <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+          <div className="item">
+            <a href="https://chanticou.github.io/Ciao-pastas-restaurant/">
               <img
-                className="image_propertiesAndYou_swiperComponent"
-                src={propertiesAndYou}
-                alt=""
-              />
+                className="image_carrusel"
+                src={chiaoLogo}
+                alt="Describe Image"
+              ></img>
+
+              <h1 className="heading">Chiao</h1>
+              <p className="duration">Proyecto CoderHouse</p>
             </a>
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="content_swiperSlide_swiperComponent">
-            {/* <Link to="/proyect/propertiesAndYou"> */}
-            <p>Properties&&you</p>
-            <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+          <div className="item">
+            <a href="#">
               <img
-                className="image_propertiesAndYou_swiperComponent"
-                src={propertiesAndYou}
-                alt=""
-              />
+                src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABQCoK53qihwVPLRxPEDX98nyYpGbxgi5cc0ZOM4iHQu7KQvtgNyaNM5PsgI0vy5g3rLPZdjGCFr1EggrCPXpL77p2H08jV0tNEmIfs_e8KUfvBJ6Ay5nM4UM1dl-58xA6t1swmautOM.webp"
+                alt="Describe Image"
+              ></img>
+              <h1 className="heading">Heading One</h1>
+              <p className="duration">Duration: 10 min</p>
             </a>
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="content_swiperSlide_swiperComponent">
-            {/* <Link to="/proyect/propertiesAndYou"> */}
-            <p>Properties&&you</p>
-            <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+          <div className="item">
+            <a href="#">
               <img
-                className="image_propertiesAndYou_swiperComponent"
-                src={propertiesAndYou}
-                alt=""
-              />
+                src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABdYtAqj8CyaJTWq5taD8Ro_UgwH3nne9QpFGVps-2J3IG-leqrfqXFii4jzZn48nPYTkrlwKQEV0R7_cEKlYBPRzdKqNODc-Oz26IL3LlLgFboXibIWXwxzeYxzuqn0I9TpARjeByw.jpg"
+                alt="Describe Image"
+              ></img>
+
+              <h1 className="heading">Heading One</h1>
+              <p className="duration">Duration: 10 min</p>
             </a>
           </div>
-        </SwiperSlide>
-      </Swiper>
+          <div className="item">
+            <a href="#">
+              <img
+                src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABbcCX42tsqGbBvO2y9CQv5-7QvYbCfoHtXsuc6NPCtZaKa4l4fBX3XWvUwG9F2A3CTsNpHVmulxBbdXKwK8Q6xGjejd9FoadGkZ7CnGrSl601TOQjzSHJ23NuIPC8j0QMGORL4uRIA.jpg"
+                alt="Describe Image"
+              ></img>
+              <h1 className="heading">Heading One</h1>
+              <p className="duration">Duration: 10 min</p>
+            </a>
+          </div>
+          <a href="#section2" className="arrow__btn right-arrow">
+            ›
+          </a>
+        </section>
+
+        <section id="section2">
+          <a href="#section1" className="arrow__btn left-arrow">
+            ‹
+          </a>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABVopDZ5Fy9_fk_HO5WxHTXKKjKhtWFupbSjuvPwfLK_vytzon4EwRUdGgYJ34JwPxOTK_NkV3aXfkULMB0Dcct-FyfqzH-X44VXuRMp4QeBHlvKwWeZFpZlGdItPzmmg4scmwhG7SQ.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABTOj1-116yVcgKWMU2dI3GFR4x0fSkiGsqtLLeLUxRR7STaksjAqBTrYlTfrB8nIGnGvXksi0ewXAhVGg6-pLxpFOIfcpjK-pf8D5xehFZo5a6vJbo4L0AGbrzglbyUoq255QBJgRQ.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABd3IBDpxbRcHXvRMFCZeKa2aHLU1P4SJtrACMS9om3yhLjqPlvNlmR_fypPxjtbsbnKaC4JZhPSpDG4r_kdxSHHAltWguMcCB-1Y1OShr2zWfUv7Whf_39fNH5ZJ3_0gxQrs0akmQjQz44_LT7jXH5LMZ7iMBAzac5IEj4m7Fn_5OWEGYnVsDsKG-QTommDooULMDF9bEw.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABXSd7bhDddcwkq9XpksoQFCHVx29Sxl_h4hb2n3F2GIt32a4XWcOnctQfgnT5qdolv8UML6_xNB5CJ89h56wueb13mYmEBr0sx5e9iLPdtVcOQAOmKXKWHHXwFvJuCUwuNnL3s8eAQwqLXPVMHMEsujM684rUGrZNF2btN2GRy5-RyEslsxZO93V2Q_H2bWs8A8oayt1h5M.webp"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABbXWODpAWqVXcmmjMA7K-2mPkQpvwCLfSdeyhVXzR8A3MSpdSEnnjf4HEJJTYC-TnktU6njTUGAxmzWEYCaJbk4v_ZeL-7QGzmkvYBjg_N-evr2XmcX-Fanoyvu_nimFP4iigPe4O3Vr_WcgplhwkDrJwPUJa84wRLrNAx3TufN5V7cWRP4indqu5HQahvgFEqfL9zjp4g.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <a href="#section3" className="arrow__btn right-arrow">
+            ›
+          </a>
+        </section>
+
+        <section id="section3">
+          <a href="#section2" className="arrow__btn left-arrow">
+            ‹
+          </a>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABRr4YxdaABuAuH_3FmSQZn7BCvLp-UUPsXE9MiYpvFP3CSUHV2zOew5oVqKqqdaOd3tbFVS0Uf67uIs7_eZydlCghg4nK0nMatRpPImABwTOhnNzCLCxdKrua7pPIcPCZqBYTeAO5g.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABTyWK1MKaw8GcObtz47R2Tj7wkLJ7qQu9tk6TVpcoyxpzD4B-zZ569bQ5vGrREBL-MWFkGilXUwy7tCDaj2XOGkUB4RsbbFAmp9NgSr6lygMpUGNHSlyfrFbUORsRkrxSIoh_ggOvg.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABY7NwkWEJIfXsn6t3Li4bGSCQ1nEErPisI5ZZtXlC-_VRBZOUrhWK5X3vt3t6SR_cpgVhCwxgQqFFDJhk62Kk8hawOnYGZMr0LKeLczMFV2zalCFjkcdLksvT4HB2LEi6LFyruyk3Uu0LmNGsHfC2A8Bly60smr_3sDbz4RruXcklPOG1qYq9wUVu3zfaiwNvqmG4b8aFA.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABemXHOga9feFnOux6I2YyACBD94wvM7N3vcTGIfMpQ_BcaXeoeM9XyzdVdamKtxt0SHXZfvsl6czcp3E48tXMLtHBxuQsh1BjHtPGgJDZ81je_FjItINiqzLtir0A30s_e4KR8G3d7AYAPDjZVOY97bNpzNqtkcHcGp7fGnJByVCps1uLfG9U9tK3Ma1A_3JbRt0NiT2_Q.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+          <div className="item">
+            <img
+              src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABVxuRB932hvre-XP0gh6ar5ztoR3Oe3QjKHkyvcDnRak2MKXOrx5H7mFQSvggefMFOppwEs7ZCCpiqrJ_CYGvtvYB9NpU4SWUtNO6uV2u-DTID267AcHjHcGvGBQJ1ufddDkxcGOZyi5MlOQ5QUmBun4652FbYUnib3zMYQDgcna_Pvz8y_HO5fbokxezrRR1JZAAiqFSQ.jpg"
+              alt="Describe Image"
+            ></img>
+          </div>
+
+          <a href="#section1" className="arrow__btn right-arrow">
+            ›
+          </a>
+        </section>
+      </div>
     </>
   );
 }
+
+// <Swiper
+// slidesPerView={3}
+// spaceBetween={30}
+// pagination={{
+//   clickable: true,
+// }}
+// modules={[Pagination]}
+// className="mySwiper"
+// >
+// <SwiperSlide>
+//   <div className="content_swiperSlide_swiperComponent">
+//     {/* <Link to="/proyect/propertiesAndYou"> */}
+//     {/* <p>Properties&&you</p> */}
+//     <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+//       <img
+//         className="image_propertiesAndYou_swiperComponent"
+//         src={propertiesAndYou}
+//         alt=""
+//       />
+//     </a>
+//   </div>
+// </SwiperSlide>
+
+// <SwiperSlide>
+//   <div className="content_swiperSlide_swiperComponent">
+//     {/* <Link to="/proyect/propertiesAndYou"> */}
+//     {/* <p>Properties&&you</p> */}
+//     <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+//       <img
+//         className="image_propertiesAndYou_swiperComponent"
+//         src={propertiesAndYou}
+//         alt=""
+//       />
+//     </a>
+//   </div>
+// </SwiperSlide>
+// <SwiperSlide>
+//   <div className="content_swiperSlide_swiperComponent">
+//     {/* <Link to="/proyect/propertiesAndYou"> */}
+//     {/* <p>Properties&&you</p> */}
+//     <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+//       <img
+//         className="image_propertiesAndYou_swiperComponent"
+//         src={propertiesAndYou}
+//         alt=""
+//       />
+//     </a>
+//   </div>
+// </SwiperSlide>
+// <SwiperSlide>
+//   <div className="content_swiperSlide_swiperComponent">
+//     {/* <Link to="/proyect/propertiesAndYou"> */}
+//     {/* <p>Properties&&you</p> */}
+//     <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+//       <img
+//         className="image_propertiesAndYou_swiperComponent"
+//         src={propertiesAndYou}
+//         alt=""
+//       />
+//     </a>
+//   </div>
+// </SwiperSlide>
+// <SwiperSlide>
+//   <div className="content_swiperSlide_swiperComponent">
+//     {/* <Link to="/proyect/propertiesAndYou"> */}
+//     {/* <p>Properties&&you</p> */}
+//     <a href={"https://proyecto-final-frontend-lyart.vercel.app/"}>
+//       <img
+//         className="image_propertiesAndYou_swiperComponent"
+//         src={propertiesAndYou}
+//         alt=""
+//       />
+//     </a>
+//   </div>
+// </SwiperSlide>
+// </Swiper>
